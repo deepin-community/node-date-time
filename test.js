002a7f052f1cf2fb5d3c0484a22717f5ff68cf35
Deepin Developer <deepin-dev@deepin.org>
2022-10-13 13:17:55 +0800

    feat: Init commit
    init node-date-time
    
    Log:

---
+++ test.js
@@ -0,0 +1,29 @@
+import test from 'ava';
+import dateTime from '.';
+
+test('main', t => {
+	t.regex(dateTime(), /^[\d-]+ [\d:]+$/);
+	t.regex(dateTime({date: new Date()}), /^[\d-]+ [\d:]+$/);
+});
+
+test('showTimeZone option', t => {
+	t.regex(dateTime({showTimeZone: true}), /^[\d-]+ [\d:]+ UTC[-+][\d:]+$/);
+
+	t.regex(dateTime({
+		local: false,
+		showTimeZone: true
+	}), /^[\d-]+ [\d:]+ UTC$/);
+});
+
+test('showMilliseconds option', t => {
+	t.regex(dateTime({
+		local: false,
+		showMilliseconds: true
+	}), /^[\d-]+ [\d:]+ \d+ms$/);
+
+	t.regex(dateTime({
+		local: false,
+		showMilliseconds: true,
+		showTimeZone: true
+	}), /^[\d-]+ [\d:]+ \d+ms UTC$/);
+});
